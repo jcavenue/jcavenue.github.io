2e4327ad1c0cfaf97f0a29b2db93d716aa5072f5
Suzaku <jcfababeir09@gmail.com>
2021-08-15 12:01:09 +0800

    Initial design for Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,35 @@
-import { withRouter } from "react-router-dom";
+import { withRouter, Link } from "react-router-dom";
+import { MDBContainer, MDBRow, MDBCol, MDBTypography, MDBBtn } from 'mdb-react-ui-kit';
 
 const Home = () => {
 
 	return (
-		<div className='p-5 text-center bg-DDDDDD' style={{ height: 500,}}>
-        <h1 className='mb-3'>Heading</h1>
-        <h4 className='mb-3'>Subheading</h4>
+		<div className='p-5 bg-DDDDDD ' style={{ height: 500,}}>
+			<MDBContainer>
+				<MDBRow>
+					<MDBCol lg="6" xs="12" className="p-5 text-md-start text-center">
+						<MDBTypography variant='h1' className="display-5">Hey! I'm JC</MDBTypography>
+						<p className="fst-italic fs-5 mb-4"> Front-end Developer</p>
+						<p className="fs-4 mb-3">
+							Building and Creating <br/>
+							User-friendly Website
+						</p>
+						<p>
+							Want to know more about me?
+						</p>
+						<p>
+							<Link to="/Project">
+								<MDBBtn color='success'>Click Here</MDBBtn>
+							</Link>
+						</p>
+					</MDBCol>
+					<MDBCol lg="6" xs="12">
+						
+					</MDBCol>
+				</MDBRow>
+			</MDBContainer>
     </div>
+		
 	)
 }
 
