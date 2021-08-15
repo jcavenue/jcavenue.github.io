34aea0aa9ce214b746553ecd2c5a70a0249e8368
Suzaku <jcfababeir09@gmail.com>
2021-08-15 10:30:29 +0800

    Add design on Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,39 @@
-const Footer = () => {
-	return (
-		<div>
-			<a href="https://github.com/jcavenue" target="_blank" rel="noreferrer">Github</a> 
-			<a href="https://linkedin.com/in/jcavenue" target="_blank" rel="noreferrer">linkedin</a>
-		</div>
-	)
-}
+import React from 'react';
+import { MDBFooter, MDBContainer, MDBRow, MDBCol} from 'mdb-react-ui-kit';
 
-export default Footer;
+export default function Footer() {
+  return (
+    <MDBFooter backgroundColor='light' className='text-center text-lg-left'>
+      <MDBContainer className='p-4'>
+        <MDBRow>
+          <MDBCol lg='6' md='12' className='mb-4 mb-md-0'>
+            <h5 className='text-uppercase'>Footer text</h5>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis molestias.
+              Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam voluptatem veniam,
+              est atque cumque eum delectus sint!
+            </p>
+          </MDBCol>
+
+          <MDBCol lg='6' md='12' className='mb-4 mb-md-0'>
+            <h5 className='text-uppercase'>Footer text</h5>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis molestias.
+              Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam voluptatem veniam,
+              est atque cumque eum delectus sint!
+            </p>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+
+      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        &copy; {new Date().getFullYear()} Copyright:{' '}
+        <a className='text-dark' target="_blank" rel="noreferrer nofollow" href='https://jcavenue.github.io/'>
+          jcavenue.github.io
+        </a>
+      </div>
+    </MDBFooter>
+  );
+}
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,10 +3,9 @@ import { withRouter } from "react-router-dom";
 const Home = () => {
 
 	return (
-		<div className='p-5 text-center bg-light' style={{ height: 900,}}>
+		<div className='p-5 text-center bg-DDDDDD' style={{ height: 500,}}>
         <h1 className='mb-3'>Heading</h1>
         <h4 className='mb-3'>Subheading</h4>
-        
     </div>
 	)
 }
