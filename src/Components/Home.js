1ed9096fe4a07792e73f5f93d9bab1241f241e15
Suzaku <jcfababeir09@gmail.com>
2021-08-27 19:52:29 +0800

    Refactor Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,17 +5,19 @@ import HomeBG from '../Assets/img/HomeBg.png'
 const Home = () => {
 
 	return (
-		<div className='pt-md-5 px-lg-5 '>
-			<MDBContainer>
+			<MDBContainer className='pt-md-5 px-lg-5 '>
 				<MDBRow>
 					<MDBCol lg="5" xs="12" className="p-0 pt-5 pb-3 text-lg-start text-center">
 						<MDBTypography variant='h1' style={{fontSize: '60px'}}>Hey! I'm JC</MDBTypography>
+						
 						<p className="fst-italic fs-5 mb-4"> Front-end Developer</p>
 						<p className="fs-3 mb-3">
 							Building and Creating <br/>
 							User-friendly Website
 						</p>
+
 						<p>Want to know more about me?</p>
+						
 						<p>
 							<Link to="/About">
 								<MDBBtn rounded color='success'>Click Here</MDBBtn>
@@ -27,7 +29,6 @@ const Home = () => {
 					</MDBCol>
 				</MDBRow>
 			</MDBContainer>
-    </div>
 	)
 }
 
